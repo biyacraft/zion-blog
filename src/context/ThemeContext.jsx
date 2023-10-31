94121af2e9e8fba9b7ce5108e9c1b7905715be08
abbyyT <abitesfahunn@gmail.com>
2023-10-31 22:10:58 +0300

    Update ThemeContext.jsx

--- src/context/ThemeContext.jsx
+++ src/context/ThemeContext.jsx
@@ -3,12 +3,18 @@ import { createContext } from "react"
 import {useState} from "react"
 export const ThemeContext = createContext();
 
+
+
 const getFromLocalStorage = () => {
     if (typeof window !== undefined) {
     const value = localStorage.getItem("theme");
     return value || "light";
     }
 };
+useEffect(() => {
+    setMounted(true);
+  }, []);
+
 export const ThemeContextProvider = ({children}) => {
     const [theme, setTheme] = useState(() =>{
         return getFromLocalStorage();
